d6d598ae3c050afa907586e6e6f81619ddbfe221
jwuming <41560016+zhaojunchen@users.noreply.github.com>
2022-08-31 11:39:34 +0800

    feat: 样式定制

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -21,3 +21,15 @@ val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+
+val pink100 = Color(0xFFFFF1F1)
+val pink900 = Color(0xFF3F2C2C)
+val white = Color(0xFFFFFFFF)
+
+// using copy get a different alpha color
+val white85 = white.copy(alpha = 0.85f)
+val white15 = white.copy(alpha = 0.15f)
+val gray = Color(0xFF232323)
+
+val green900 = Color(0xFF2D3B2D)
+val green300 = Color(0xFFB8C9B8)
--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -23,21 +23,46 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        letterSpacing = 0.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = (0.15).sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.sp
+    ),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-        /* Other default text styles to override
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 1.sp
     ),
     caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = 12.sp,
+        letterSpacing = 0.sp
+    ),
 )
